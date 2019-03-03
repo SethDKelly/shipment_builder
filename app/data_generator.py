eb73d87d5b69ae977b7a8a057bf288dcf287bf5a
SethDKelly <ollioxenhomefree@gmail.com>
2019-03-03 16:58:02 -0600

    Built csv and DataFrame generator package

--- app/data_generator.py
+++ app/data_generator.py
@@ -1,11 +1,13 @@
-def data_generator():
+from app.clean import clean_csv()
+import pandas as pd
+
+def dataframe_generator():
     
     # Builds anywhere from 150-250 rows of data that matches what comes from items.csv
     # Creates random 'keys' that it merges with the stock data based on index
-    # returns the data as... pandas DataFrame
+    # returns the data as pandas DataFrame
     
     import random
-    import pandas as pd
     
     return (pd.DataFrame(data = {'key': [random.randint(0,199) for x in range(random.randint(150,250))]})
               .merge(clean_csv(), 
@@ -16,11 +18,25 @@ def data_generator():
               .reset_index(drop=True)
            )
 
-'''
-Future implmentations will return this as .csv data that will be required to be loaded in
-"app/data/tmp/<DataName>.csv"
-
-To do this data_generator will take an integer parameter
-Create csv's from the integer, 3 will make 3 .csv files
-check what the name is and build the names dynamically by what's already stored
-'''
+def csv_generator():    
+    # Builds anywhere from 150-250 rows of data that matches what comes from items.csv
+    # Creates random 'keys' that it merges with the stock data based on index
+    # returns the data as .csv's
+    
+    import random
+    import glob
+    import re
+    
+    count = sorted([int(re.sub("[^0-9]", "", files)) for files in glob.glob("app/data/tmp/*.csv")])[-1] + 1
+    if not count: 
+        count = '1'        
+    
+    return (pd.DataFrame(data = {'key': [random.randint(0,199) for x in range(random.randint(150,250))]})
+              .merge(clean_csv(), 
+                     left_on='key', 
+                     right_index=True)
+              .drop('key',
+                    axis = 1)
+              .reset_index(drop=True)
+           ).to_csv("app/data/tmp/items"+str(count)+".csv", 
+                    index=False)
