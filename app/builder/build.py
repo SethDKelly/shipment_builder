8b2d3140f7da97d208f33c91fd0c91a301383d9a
SethDKelly <ollioxenhomefree@gmail.com>
2019-03-02 20:20:52 -0600

    Change how summary is built Adjusted for when item_group is single item_group

--- app/builder/build.py
+++ app/builder/build.py
@@ -2,6 +2,13 @@ from app.builder import generate_shipment_id as gen_id
 import pandas as pd
 
 def items():
+    
+    """
+    Check app/data/tmp/ for any .csv data
+    Append all the data and return the result
+    result will be a single DataFrame
+    """
+    
     import glob
     
     # It's nice to assume clean data, and to be right for once
@@ -53,16 +60,20 @@ def summary(shipments):
     
     # Build initial summaries based on items and cubic volume in feet
     data = {'Total Items' : len(shipments.item_id.values),
-            'Total Cubic Volume in Feet' : (shipments.cubic_volume_ft.values.sum()),
-            'Total Item Groups' : len(shipments.item_group.unique())}
+            'Total Cubic Volume in Feet' : (shipments.cubic_volume_ft.values.sum())}
     
+    # When the table only contains a single item don't include it in the summary
+    if shipments.item_group.unique() > 1:
+        data['Total Item Groups'] = len(shipments.item_group.unique())
+        
     # Check for shipment id and build additional shipment summaries
     if 'shipment_id' in shipments.keys() :
         data['Total Shipments'] = len(shipments.shipment_id.unique())
         data['Shipment Item Ratio'] = len(shipments.item_id.values) / len(shipments.shipment_id.unique())
         data['Cubic Volume not Utilized'] = (1.58*len(shipments.shipment_id.unique()) - shipments.cubic_volume_ft.values.sum())
-        data['Percent Cubic Volume not Utilized'] = round(((1.58 * len(shipments.shipment_id.unique()) - shipments.cubic_volume_ft.values.sum()) / 
-                                                     shipments.cubic_volume_ft.values.sum()) * 100, 2)
+        data['Percent Cubic Volume not Utilized'] = round(((1.58 * len(shipments.shipment_id.unique()) -
+                                                            shipments.cubic_volume_ft.values.sum()) / 
+                                                           shipments.cubic_volume_ft.values.sum()) * 100, 2)
     # return resulting summary as a DataFrame
     return (pd.DataFrame(data, 
                          index=['Details'])
